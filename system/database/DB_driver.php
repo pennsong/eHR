a5bef8111746d92836f3b99d5032e3b9ccb8e511
pennProMac <pennsong07@gmail.com>
2012-05-31 00:14:54 +0800

    system/database/drivers/mysqli/mysqli_result.php add function: next_result() free_all() store_result() -------- system/database/DB_driver.php improve mysqli driver:
    changed in function query($sql, $binds = FALSE, $return_object = TRUE):
    
    [* @return	mixed] to [* @return	CI_DB_mysqli_result]
    
    
    add function:
    multi_query($sql, $binds = FALSE, $return_object = TRUE)

--- system/database/DB_driver.php
+++ system/database/DB_driver.php
@@ -78,7 +78,7 @@ class CI_DB_driver {
 	 *
 	 * @param array
 	 */
-	function __construct($params)
+	function CI_DB_driver($params)
 	{
 		if (is_array($params))
 		{
@@ -218,7 +218,7 @@ class CI_DB_driver {
 
 		// Some DBs have functions that return the version, and don't run special
 		// SQL queries per se. In these instances, just return the result.
-		$driver_version_exceptions = array('oci8', 'sqlite', 'cubrid');
+		$driver_version_exceptions = array('oci8', 'sqlite');
 
 		if (in_array($this->dbdriver, $driver_version_exceptions))
 		{
@@ -232,21 +232,20 @@ class CI_DB_driver {
 	}
 
 	// --------------------------------------------------------------------
-
 	/**
-	 * Execute the query
-	 *
-	 * Accepts an SQL string as input and returns a result object upon
-	 * successful execution of a "read" type query.  Returns boolean TRUE
-	 * upon successful execution of a "write" type query. Returns boolean
-	 * FALSE upon failure, and if the $db_debug variable is set to TRUE
-	 * will raise an error.
-	 *
-	 * @access	public
-	 * @param	string	An SQL query string
-	 * @param	array	An array of binding data
-	 * @return	mixed
-	 */
+	* Execute the query
+	*
+	* Accepts an SQL string as input and returns a result object upon
+	* successful execution of a "read" type query.  Returns boolean TRUE
+	* upon successful execution of a "write" type query. Returns boolean
+	* FALSE upon failure, and if the $db_debug variable is set to TRUE
+	* will raise an error.
+	*
+	* @access	public
+	* @param	string	An SQL query string
+	* @param	array	An array of binding data
+	* @return	CI_DB_mysqli_result
+	*/
 	function query($sql, $binds = FALSE, $return_object = TRUE)
 	{
 		if ($sql == '')
@@ -322,12 +321,12 @@ class CI_DB_driver {
 				// Log and display errors
 				log_message('error', 'Query error: '.$error_msg);
 				return $this->display_error(
-										array(
+				array(
 												'Error Number: '.$error_no,
-												$error_msg,
-												$sql
-											)
-										);
+				$error_msg,
+				$sql
+				)
+				);
 			}
 
 			return FALSE;
@@ -409,6 +408,193 @@ class CI_DB_driver {
 
 		return $RES;
 	}
+	// --------------------------------------------------------------------
+	/**
+	* Execute the mysql multi_query
+	*
+	* Accepts an SQL string as input and returns a result object upon
+	* successful execution of a "read" type query.  Returns boolean TRUE
+	* upon successful execution of a "write" type query. Returns boolean
+	* FALSE upon failure, and if the $db_debug variable is set to TRUE
+	* will raise an error.
+	*
+	* @access	public
+	* @param	string	An SQL query string
+	* @param	array	An array of binding data
+	* @return	CI_DB_mysqli_result
+	*/
+	function multi_query($sql, $binds = FALSE, $return_object = TRUE)
+	{
+		if ($sql == '')
+		{
+			if ($this->db_debug)
+			{
+				log_message('error', 'Invalid query: '.$sql);
+				return $this->display_error('db_invalid_query');
+			}
+			return FALSE;
+		}
+
+		// Verify table prefix and replace if necessary
+		if ( ($this->dbprefix != '' AND $this->swap_pre != '') AND ($this->dbprefix != $this->swap_pre) )
+		{
+			$sql = preg_replace("/(\W)".$this->swap_pre."(\S+?)/", "\\1".$this->dbprefix."\\2", $sql);
+		}
+
+		// Is query caching enabled?  If the query is a "read type"
+		// we will load the caching class and return the previously
+		// cached query if it exists
+		if ($this->cache_on == TRUE AND stristr($sql, 'SELECT'))
+		{
+			if ($this->_cache_init())
+			{
+				$this->load_rdriver();
+				if (FALSE !== ($cache = $this->CACHE->read($sql)))
+				{
+					return $cache;
+				}
+			}
+		}
+
+		// Compile binds if needed
+		if ($binds !== FALSE)
+		{
+			$sql = $this->compile_binds($sql, $binds);
+		}
+
+		// Save the  query for debugging
+		if ($this->save_queries == TRUE)
+		{
+			$this->queries[] = $sql;
+		}
+
+		// Start the Query Timer
+		$time_start = list($sm, $ss) = explode(' ', microtime());
+
+		// Run the Query
+		$sql = $this->_prep_query($sql);
+		if (FALSE === ($this->result_id = $this->conn_id->multi_query($sql)))
+		{
+			if ($this->save_queries == TRUE)
+			{
+				$this->query_times[] = 0;
+			}
+
+			// This will trigger a rollback if transactions are being used
+			$this->_trans_status = FALSE;
+
+			if ($this->db_debug)
+			{
+				// grab the error number and message now, as we might run some
+				// additional queries before displaying the error
+				$error_no = $this->_error_number();
+				$error_msg = $this->_error_message();
+
+				// We call this function in order to roll-back queries
+				// if transactions are enabled.  If we don't call this here
+				// the error message will trigger an exit, causing the
+				// transactions to remain in limbo.
+				$this->trans_complete();
+
+				// Log and display errors
+				log_message('error', 'Query error: '.$error_msg);
+				return $this->display_error(
+				array(
+												'Error Number: '.$error_no,
+				$error_msg,
+				$sql
+				)
+				);
+			}
+
+			return FALSE;
+		}
+
+		// Stop and aggregate the query time results
+		$time_end = list($em, $es) = explode(' ', microtime());
+		$this->benchmark += ($em + $es) - ($sm + $ss);
+
+		if ($this->save_queries == TRUE)
+		{
+			$this->query_times[] = ($em + $es) - ($sm + $ss);
+		}
+
+		// Increment the query counter
+		$this->query_count++;
+
+		// Was the query a "write" type?
+		// If so we'll simply return true
+		if ($this->is_write_type($sql) === TRUE)
+		{
+			// If caching is enabled we'll auto-cleanup any
+			// existing files related to this particular URI
+			if ($this->cache_on == TRUE AND $this->cache_autodel == TRUE AND $this->_cache_init())
+			{
+				$this->CACHE->delete();
+			}
+
+			return TRUE;
+		}
+
+		// Return TRUE if we don't need to create a result object
+		// Currently only the Oracle driver uses this when stored
+		// procedures are used
+		if ($return_object !== TRUE)
+		{
+			return TRUE;
+		}
+
+		// Load and instantiate the result driver
+
+		$driver			= $this->load_rdriver();
+		$RES			= new $driver();
+		$RES->conn_id	= $this->conn_id;
+
+		//penn:check it's a query or a multi_query
+		if ($this->result_id instanceof mysqli_result)
+		{
+			$RES->result_id	= $this->result_id;
+		}
+		else
+		{
+			$RES->result_id = $this->conn_id->store_result();
+		}
+
+		if ($this->dbdriver == 'oci8')
+		{
+			$RES->stmt_id		= $this->stmt_id;
+			$RES->curs_id		= NULL;
+			$RES->limit_used	= $this->limit_used;
+			$this->stmt_id		= FALSE;
+		}
+
+		// oci8 vars must be set before calling this
+		$RES->num_rows	= $RES->num_rows();
+
+		// Is query caching enabled?  If so, we'll serialize the
+		// result object and save it to a cache file.
+		if ($this->cache_on == TRUE AND $this->_cache_init())
+		{
+			// We'll create a new instance of the result object
+			// only without the platform specific driver since
+			// we can't use it with cached data (the query result
+			// resource ID won't be any good once we've cached the
+			// result object, so we'll have to compile the data
+			// and save it)
+			$CR = new CI_DB_result();
+			$CR->num_rows		= $RES->num_rows();
+			$CR->result_object	= $RES->result_object();
+			$CR->result_array	= $RES->result_array();
+
+			// Reset these since cached objects can not utilize resource IDs.
+			$CR->conn_id		= NULL;
+			$CR->result_id		= NULL;
+
+			$this->CACHE->write($sql, $CR);
+		}
+
+		return $RES;
+	}
 
 	// --------------------------------------------------------------------
 
@@ -424,8 +610,8 @@ class CI_DB_driver {
 
 		if ( ! class_exists($driver))
 		{
-			include_once(BASEPATH.'database/DB_result.php');
-			include_once(BASEPATH.'database/drivers/'.$this->dbdriver.'/'.$this->dbdriver.'_result.php');
+			include_once(BASEPATH.'database/DB_result'.EXT);
+			include_once(BASEPATH.'database/drivers/'.$this->dbdriver.'/'.$this->dbdriver.'_result'.EXT);
 		}
 
 		return $driver;
@@ -1115,7 +1301,7 @@ class CI_DB_driver {
 
 		if ( ! class_exists('CI_DB_Cache'))
 		{
-			if ( ! @include(BASEPATH.'database/DB_cache.php'))
+			if ( ! @include(BASEPATH.'database/DB_cache'.EXT))
 			{
 				return $this->cache_off();
 			}
--- system/database/drivers/mysqli/mysqli_result.php
+++ system/database/drivers/mysqli/mysqli_result.php
@@ -84,26 +84,21 @@ class CI_DB_mysqli_result extends CI_DB_result {
 	function field_data()
 	{
 		$retval = array();
-		while ($field = mysqli_fetch_object($this->result_id))
+		while ($field = mysqli_fetch_field($this->result_id))
 		{
-			preg_match('/([a-zA-Z]+)(\(\d+\))?/', $field->Type, $matches);
-
-			$type = (array_key_exists(1, $matches)) ? $matches[1] : NULL;
-			$length = (array_key_exists(2, $matches)) ? preg_replace('/[^\d]/', '', $matches[2]) : NULL;
-
 			$F				= new stdClass();
-			$F->name		= $field->Field;
-			$F->type		= $type;
-			$F->default		= $field->Default;
-			$F->max_length	= $length;
-			$F->primary_key = ( $field->Key == 'PRI' ? 1 : 0 );
+			$F->name		= $field->name;
+			$F->type		= $field->type;
+			$F->default		= $field->def;
+			$F->max_length	= $field->max_length;
+			$F->primary_key = ($field->flags & MYSQLI_PRI_KEY_FLAG) ? 1 : 0;
 
 			$retval[] = $F;
 		}
 
 		return $retval;
 	}
-	
+
 	// --------------------------------------------------------------------
 
 	/**
@@ -166,14 +161,68 @@ class CI_DB_mysqli_result extends CI_DB_result {
 	{
 		return mysqli_fetch_object($this->result_id);
 	}
-	
+
+	// --------------------------------------------------------------------
+	/**
+	* Read the next result
+	*
+	* @return	boolean
+	*/
+	function next_result()
+	{
+		if (is_object($this->conn_id))
+		{
+			return mysqli_next_result($this->conn_id);
+		}
+	}
+	// --------------------------------------------------------------------
+	/**
+	* Read the next result
+	*
+	* @return	CI_DB_mysqli_result
+	*/
+	function store_result()
+	{
+		// Load and instantiate the result driver
+		$driver			= 'CI_DB_mysqli_result';
+		$RES			= new $driver();
+		$RES->conn_id	= CI_Controller::get_instance()->db->conn_id;
+		$RES->result_id = mysqli_store_result(CI_Controller::get_instance()->db->conn_id);
+
+		// oci8 vars must be set before calling this
+		$RES->num_rows	= $RES->num_rows();
+
+		// Is query caching enabled?  If so, we'll serialize the
+		// result object and save it to a cache file.
+
+		if (CI_Controller::get_instance()->db->cache_on == TRUE AND CI_Controller::get_instance()->db->_cache_init())
+		{
+			// We'll create a new instance of the result object
+			// only without the platform specific driver since
+			// we can't use it with cached data (the query result
+			// resource ID won't be any good once we've cached the
+			// result object, so we'll have to compile the data
+			// and save it)
+			$CR = new CI_DB_result();
+			$CR->num_rows		= $RES->num_rows();
+			$CR->result_object	= $RES->result_object();
+			$CR->result_array	= $RES->result_array();
+
+			// Reset these since cached objects can not utilize resource IDs.
+			$CR->conn_id		= NULL;
+			$CR->result_id		= NULL;
+
+			$this->CACHE->write($sql, $CR);
+		}
+
+		return $RES;
+	}
 	// --------------------------------------------------------------------
 
 	/**
-	 * Free the all result
+	 * free result sets
 	 *
-	 * @return boolean
-	 * @author penn
+	 * @return 	null
 	 */
 	function free_all()
 	{
