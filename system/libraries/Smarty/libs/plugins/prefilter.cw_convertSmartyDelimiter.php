1b32517b50f0feb0afdcea04c37a41a61b98f967
pennProMac <pennsong07@gmail.com>
2012-06-04 20:06:54 +0800

    add /*{smarty code}*/ support in js

--- system/libraries/Smarty/libs/plugins/prefilter.cw_convertSmartyDelimiter.php
+++ system/libraries/Smarty/libs/plugins/prefilter.cw_convertSmartyDelimiter.php
@@ -1,7 +1,7 @@
 <?php
 function smarty_prefilter_cw_convertSmartyDelimiter($tpl_source, Smarty_Internal_Template $template)
 {
-	return preg_replace("/(<!--)(\{.*\})(-->)/U",'${2}',$tpl_source);
+	return preg_replace("/(<!--|\/\*)(\{.*\})(-->|\*\/)/U",'${2}',$tpl_source);
 }
 
 ?>
