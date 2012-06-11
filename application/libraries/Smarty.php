daeaf57225331614c77c2a546cf81fe85bded1ae
pennProMac <pennsong07@gmail.com>
2012-06-11 21:34:42 +0800

    improve Smarty.php: can use CI in tpl file to refer to codeIgniter current instance

--- application/libraries/Smarty.php
+++ application/libraries/Smarty.php
@@ -10,19 +10,23 @@ if (!defined('BASEPATH'))
  * @author		Kepler Gelotte
  * @link		http://www.coolphptools.com/codeigniter-smarty
  */
-require_once (BASEPATH . 'libraries/Smarty/libs/Smarty.class.php');
+require_once (BASEPATH.'libraries/Smarty/libs/Smarty.class.php');
 class CI_Smarty extends Smarty
 {
 	function __construct()
 	{
 		parent::__construct();
-		$this->compile_dir = APPPATH . "views/templates_c/";
-		$this->template_dir = APPPATH . "views/templates/";
+		$CI = &get_instance();
+		$this->assign('CI', $CI);
+		$this->compile_dir = APPPATH."views/templates_c/";
+		$this->template_dir = APPPATH."views/templates/";
 		$this->assign('APPPATH', APPPATH);
 		$this->assign('BASEPATH', BASEPATH);
 		$this->autoload_filters = array('pre' => array('cw_convertSmartyDelimiter'));
 		log_message('debug', "Smarty Class Initialized");
 	}
+
 }
 
 /* End of file Smarty.php */
+ 
